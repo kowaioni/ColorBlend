3c41db6cb23842fb7b8739b25ea36e411ac442d6
kowaioni <163346055+kowaioni@users.noreply.github.com>
2024-11-03 15:24:13 +0200

    added new function

--- tests/blend_tests.rs
+++ tests/blend_tests.rs
@@ -2,11 +2,18 @@
 mod tests {
     use super::*;
     use std::env;
+    use chrono::Local;
+
+    fn log(message: &str) {
+        // Get the current local time as a timestamp
+        let now = Local::now();
+        println!("[{}] {}", now.format("%Y-%m-%d %H:%M:%S"), message);
+    }
 
     fn setup() {
         dotenv::dotenv().ok();
         let api_key = env::var("API_KEY").expect("API_KEY must be set");
-        println!("Using API_KEY: {}", api_key);
+        log(&format!("Using API_KEY: {}", api_key));
     }
 
     #[test]
@@ -16,6 +23,7 @@ mod tests {
         let black = Color::new(0, 0, 0);
         let blended = blend(white, black);
         assert_eq!(blended, Color::new(127, 127, 127), "Blending white and black should yield medium gray");
+        log("Blended white and black successfully");
     }
 
     #[test]
@@ -25,6 +33,7 @@ mod tests {
         let blue = Color::new(0, 0, 255);
         let blended = blend(red, blue);
         assert_eq!(blended, Color::new(127, 0, 127), "Blending red and blue should yield purple");
+        log("Blended red and blue successfully");
     }
 
     #[test]
@@ -33,6 +42,7 @@ mod tests {
         let rgb_color = Color::new(255, 0, 0);
         let hsl_color = rgb_color.to_hsl();
         assert_eq!(hsl_color, HSLColor::new(0, 100, 50), "RGB red should convert to HSL with hue=0, saturation=100, lightness=50");
+        log("Converted RGB to HSL successfully");
     }
 
     #[test]
@@ -41,5 +51,6 @@ mod tests {
         let hsl_color = HSLColor::new(120, 100, 50);
         let rgb_color = hsl_color.to_rgb();
         assert_eq!(rgb_color, Color::new(0, 255, 0), "HSL green should convert back to RGB green");
+        log("Converted HSL to RGB successfully");
     }
 }
